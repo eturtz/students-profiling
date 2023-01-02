684dc083262980c7d993d2d6b72c30f97a887cc8
eturtz <eturtz@gmail.com>
2023-01-02 19:41:37 +0200

    .

---
+++ Main.py
@@ -0,0 +1,29 @@
+from youtubesearchpython import VideosSearch
+
+def main():
+    hobbiesAndIntrests ={
+        1 : "painting",
+        2 : "sculpting",
+        3 : "lego",
+        4 : "photography",
+        5 : "music",
+        11: "soccer",
+        12: "basketball",
+        13: "surfing",
+        14: "tennis",
+        15 : "dancing",
+        16:  "hiking",
+        17: "workout",
+        18: "trips",
+        19: "walking",
+        20: "fishing",
+        21: "scouts",
+        24 : "Krembo Wings"
+    }
+
+def GetFromYoutube(hobby:str,amount:int):
+
+
+
+if __name__ == '__main__':
+    main()
